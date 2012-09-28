4bbf2ae8a4135d9edaf53ec6c2e7f50152e9b6fa
Michael Hays <dancingplatypus@gmail.com>
2012-09-28 12:48:17 -0400

    Transferred private changes to the fork.  Updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This task converts all images found within a stylersheet (those within a `url( ... )` declaration) into base64-encoded data URI strings.
+This task converts all data found within a stylesheet (those within a `url( ... )` declaration) into base64-encoded data URI strings.  This includes images and fonts.
 
 ## Features
 
@@ -6,6 +6,7 @@ This task converts all images found within a stylersheet (those within a `url( .
 * Ability to specify a size limit. Default is 32kb, or IE8's limit.
 * Existing data URIs will be ignored.
 * Skip specific images by specifying a directive comment.
+* Ability to purge images that have been encoded
 * Includes two helpers: `encode_stylesheet` to encode a stylesheet, and `encode_image` to encode an image.
 
 ## Getting Started
@@ -29,7 +30,8 @@ grunt.initConfig({
   imageEmbed: {
     dist: {
       src: [ "css/styles.css" ],
-      dest: "css/output.css"
+      dest: "css/output.css",
+      deleteAfterEncoding : false
     }
   }
 });
@@ -41,6 +43,7 @@ ImageEmbed can be customized by specifying the following options:
 
 * `maxImageSize`: The maximum size of the base64 string in bytes. This defaults to `32768`, or IE8's limit. Set this to `0` to remove the limit and allow any size string.
 * `baseDir`: If you have absolute image paths in your stylesheet, the path specified in this option will be used as the base directory.
+* `deleteAfterEncoding`: Set this to true to delete images after they've been encoded.  You'll want to do this in a staging area, and not in your source directories.  Be careful.
 
 ### Skipping Images
 
@@ -50,7 +53,7 @@ Specify that an image should be skipped by adding the following comment *after* 
 
 ## Known Issues
 
-* Only one image per line can be read at the moment, so run this task before minifying your CSS file.
+* if you minify your file before running this task, most likely you've stripped comments.  In this case, /*ImageEmbed:skip*/ will not work.
 
 ## License
 
--- tasks/grunt-image-embed.js
+++ tasks/grunt-image-embed.js
@@ -7,257 +7,270 @@
  */
 
 module.exports = function(grunt) {
-  "use strict";
+    "use strict";
 
-  // Node modules
-  var fs = require("fs");
-  var path = require("path");
-  var mime = require("mime");
-  var url = require("url");
-  var http = require("http");
+    // Node modules
+    var fs = require("fs");
+    var path = require("path");
+    var mime = require("mime");
+    var url = require("url");
+    var http = require("http");
 
-  // Grunt utils
-  var utils = grunt.utils;
-  var file = grunt.file;
-  var _ = utils._;
-  var async = utils.async;
+    // Grunt utils
+    var utils = grunt.utils;
+    var file = grunt.file;
+    var _ = utils._;
+    var async = utils.async;
 
-  // Cache regex's
-  var rImages = /(?:url\()(.*)(?:\))(?!(?:.*)\/\*\s*ImageEmbed:skip\s*\*\/)/i;
-  var rExternal = /^http/;
-  var rData = /^data:/;
-  var rQuotes = /['"]/g;
+    // Cache regex's
+    var rImages = /([\s\S]*?)(url\(([^)]+)\))(?!\s*\/\*\s*ImageEmbed:skip\s*\*\/)|([\s\S]+)/img;
+    var rExternal = /^http/;
+    var rData = /^data:/;
+    var rQuotes = /['"]/g;
 
-  // Cache of already converted images
-  var cache = {};
+    // Cache of already converted images
+    var cache = {};
 
-  grunt.registerMultiTask("imageEmbed", "Embed images as base64 data URIs inside your stylesheets", function() {
-    var opts = this.data.options;
-    var src = this.file.src;
-    var dest = this.file.dest;
-    var tasks, done;
+    grunt.registerMultiTask("imageEmbed", "Embed images as base64 data URIs inside your stylesheets", function() {
+        var opts = this.data.options;
+        var src = this.file.src;
+        var dest = this.file.dest;
+        var tasks, done;
 
-    if(!src) {
-      grunt.fatal("Missing src property.");
-      return;
-    }
-
-    if(!dest) {
-      grunt.fatal("Missing dest property");
-      return;
-    }
-
-    done = this.async();
-
-    // Process each src file
-    tasks = file.expandFiles(src).map(function(srcFile) {
-      return function(callback) {
-        grunt.helper("encode_stylesheet", srcFile, opts, callback);
-      };
-    });
-
-    // Once all files have been processed write them out.
-    async.parallel(tasks, function(err, output) {
-      grunt.file.write(dest, output);
-      grunt.log.writeln('File "' + dest + '" created.');
-      done();
-    });
-  });
-
-  /**
-   * Takes a CSS file as input, goes through it line by line, and base64
-   * encodes any images it finds.
-   *
-   * @param srcFile Relative or absolute path to a source stylesheet file.
-   * @param opts Options object
-   * @param done Function to call once encoding has finished.
-   */
-  grunt.registerHelper("encode_stylesheet", function(srcFile, opts, done) {
-    // Shift args if no options object is specified
-    if(utils.kindOf(opts) === "function") {
-      done = opts;
-      opts = {};
-    }
-
-    var src = file.read(srcFile);
-    var result, match, img, line, tasks;
-
-    tasks = src.split(grunt.utils.linefeed).map(function(line) {
-      var result = rImages.exec(line);
-
-      return function(callback) {
-        if(!result) {
-          callback(null, line);
-          return;
+        if(!src) {
+            grunt.fatal("Missing src property.");
+            return;
         }
 
-        // The original image value
-        match = result[0];
-        
-        // Image value with trailing whitespace/quotes removed
-        img = result[1].trim().replace(rQuotes, "");
-
-        // Check to see if this image has already been converted, and if
-        // so, warn the user.
-        if(cache[img]) {
-          grunt.log.error("The image " + img + " has already been encoded elsewhere in your stylesheet. I'm going to do it again, but it's going to make your stylesheet a lot larger than it needs to be.");
+        if(!dest) {
+            grunt.fatal("Missing dest property");
+            return;
         }
 
-        // Use image from cache if this image has been processed already
-        if(cache[img]) {
-          callback(null, line.replace(match, cache[img]));
+        done = this.async();
 
-        // Otherwise encode the image
-        } else {
-          var loc = img;
+        // Process each src file
+        tasks = file.expandFiles(src).map(function(srcFile) {
+            return function(callback) {
+                grunt.helper("encode_stylesheet", srcFile, opts, callback);
+            };
+        });
 
-          // Resolve the image path relative to the CSS file
-          if(!rData.test(img) && !rExternal.test(img)) {
-            loc = img.charAt(0) === "/" ?
-              (opts.baseDir || "") + loc :
-              path.join(path.dirname(srcFile), img);
+        // Once all files have been processed write them out.
+        async.parallel(tasks, function(err, output) {
+            grunt.file.write(dest, output);
+            grunt.log.writeln('File "' + dest + '" created.');
+            done();
+        });
+    });
 
-            // If that didn't work, try finding the image relative to
-            // the current file instead.
-            if(!fs.existsSync(loc)) {
-              loc = path.resolve(__dirname + img);
+    /**
+     * Takes a CSS file as input, goes through it line by line, and base64
+     * encodes any images it finds.
+     *
+     * @param srcFile Relative or absolute path to a source stylesheet file.
+     * @param opts Options object
+     * @param done Function to call once encoding has finished.
+     */
+    grunt.registerHelper("encode_stylesheet", function(srcFile, opts, done) {
+        // Shift args if no options object is specified
+        if(utils.kindOf(opts) === "function") {
+            done = opts;
+            opts = {};
+        }
+
+        var deleteAfterEncoding = opts.deleteAfterEncoding;
+        var src = file.read(srcFile);
+        var result, match, img, line, tasks;
+
+        var group;
+        var exec = function(str) {
+            group = rImages.exec(str);
+            return group !== null;
+        };
+
+        // store css in result
+        var result = "";
+
+        while (exec(src)) {
+
+            // if there is another url to be processed, then:
+            //    group[1] will hold everything up to the url declaration
+            //    group[2] will hold the complete url declaration (useful if no encoding will take place)
+            //    group[3] will hold the contents of the url declaration
+            //    group[4] will be undefined
+            // if there is no other url to be processed, then group[1-3] will be undefined
+            //    group[4] will hold the entire string
+
+            if (group[4] == undefined)
+            {
+                result += group[1];
+                img = group[3].trim().replace(rQuotes, "");
+
+                // see if this img was already processed before...
+                if(cache[img]) {
+                    grunt.log.error("The image " + img + " has already been encoded elsewhere in your stylesheet. I'm going to do it again, but it's going to make your stylesheet a lot larger than it needs to be.");
+                    result = result += cache[img];
+                }
+                else {
+                    // process it and put it into the cache
+                    var loc = img;
+
+                    // Resolve the image path relative to the CSS file
+                    if(!rData.test(img) && !rExternal.test(img)) {
+                        loc = img.charAt(0) === "/" ?
+                            (opts.baseDir || "") + loc :
+                            path.join(path.dirname(srcFile), img);
+
+                        // If that didn't work, try finding the image relative to
+                        // the current file instead.
+                        if(!fs.existsSync(loc)) {
+                            loc = path.resolve(__dirname + img);
+                        }
+
+                        grunt.helper("encode_image", loc, opts, function(err, resp) {
+                            if (err === null) {
+                                result += "url('" + resp + "')";
+                                if (deleteAfterEncoding) {
+                                    grunt.log.writeln("deleting " + loc);
+                                    fs.unlinkSync(loc);
+                                }
+                            }
+                            else {
+                                result += group[2];
+                            }
+
+                        });
+                    }
+                }
             }
-          }
-
-          grunt.helper("encode_image", loc, opts, function(err, resp) {
-            resp = "url(" + resp + ")";
-            callback(err, line.replace(match, resp));
-          });
-        }
-      };
-    });
-
-    // Once each line has been processed...
-    async.series(tasks, function(err, result) {
-      done(err, result.join("\n"));
-    });
-  });
-
-  /**
-   * Takes an image (absolute path or remote) and base64 encodes it.
-   *
-   * @param img Absolute, resolved path to an image
-   * @param opts Options object
-   * @return A data URI string (mime type, base64 img, etc.) that a browser can interpret as an image
-   */
-  grunt.registerHelper("encode_image", function(img, opts, done) {
-    // Shift args
-    if(utils.kindOf(opts) === "function") {
-      done = opts;
-      opts = {};
-    }
-
-    // Set default, helper-specific options
-    opts = _.extend({
-      maxImageSize: 32768
-    }, opts);
-
-    var complete = function(err, encoded, cacheable) {
-      // Too long?
-      if(encoded && opts.maxImageSize && encoded.length > opts.maxImageSize) {
-        err = "Image " + img + " not encoded because it is larger than " + opts.maxImageSize + " bytes";
-      }
-
-      // Return the original source if an error occurred
-      if(err) {
-        grunt.log.error(err);
-        done(null, img);
-
-      // Otherwise cache the processed image and return it
-      } else {
-        if(cacheable !== false) {
-          cache[img] = encoded;
+            else {
+                result += group[4];
+            }
         }
 
-        done(null, encoded);
-      }
-    };
+        done(null, result);
+    });
 
-    // Already base64 encoded?
-    if(rData.test(img)) {
-      complete(null, img, false);
+    /**
+     * Takes an image (absolute path or remote) and base64 encodes it.
+     *
+     * @param img Absolute, resolved path to an image
+     * @param opts Options object
+     * @return A data URI string (mime type, base64 img, etc.) that a browser can interpret as an image
+     */
+    grunt.registerHelper("encode_image", function(img, opts, done) {
+        // Shift args
+        if(utils.kindOf(opts) === "function") {
+            done = opts;
+            opts = {};
+        }
 
-    // External URL?
-    } else if(rExternal.test(img)) {
-      fetchImage(img, complete);
+        // Set default, helper-specific options
+        opts = _.extend({
+            maxImageSize: 32768
+        }, opts);
 
-    // Local file?
-    } else {
-      // Does the image actually exist?
-      if(!fs.existsSync(img)) {
-        grunt.fail.warn(img + " does not exist");
-        complete(null, img, false);
-        return;
-      }
+        var complete = function(err, encoded, cacheable) {
+            // Too long?
+            if(encoded && opts.maxImageSize && encoded.length > opts.maxImageSize) {
+                err = img + " -- skipped (gt " + opts.maxImageSize + " bytes)";
+            }
 
-      grunt.log.writeln("Encoding file: " + img);
+            // Return the original source if an error occurred
+            if(err) {
+                grunt.log.error(err);
+                done(err, img);
 
-      // Read the file in and convert it.
-      fs.readFile(img, function(err, src) {
-        var type = mime.lookup(img);
-        var encoded = encode(type, src);
-        complete(err, encoded);
-      });
+                // Otherwise cache the processed image and return it
+            } else {
+                if(cacheable !== false) {
+                    cache[img] = encoded;
+                }
+
+                done(null, encoded);
+            }
+        };
+
+        // Already base64 encoded?
+        if(rData.test(img)) {
+            complete(null, img, false);
+
+            // External URL?
+        } else if(rExternal.test(img)) {
+            fetchImage(img, complete);
+
+            // Local file?
+        } else {
+            // Does the image actually exist?
+            if(!fs.existsSync(img)) {
+                grunt.fail.warn(img + " does not exist");
+                complete(null, img, false);
+                return;
+            }
+
+            grunt.log.writeln("Encoding file: " + img);
+
+            // Read the file in and convert it.
+            var src = fs.readFileSync(img);
+            var type = mime.lookup(img);
+            var encoded = encode(type, src);
+            complete(null, encoded);
+        }
+    });
+
+    /**
+     * Fetches a remote image and encodes it.
+     *
+     * @param img Remote path, like http://url.to/an/image.png
+     * @param done Function to call once done
+     */
+    function fetchImage(img, done) {
+        var opts = url.parse(img);
+
+        var req = http.request(opts, function(res) {
+            res.setEncoding("binary");
+
+            var mime = res.headers["content-type"];
+            var data = "";
+
+            // Bail if we get anything other than 200
+            if(res.statusCode !== 200) {
+                done("Unable to convert " + img + " because the URL did not return an image. Staus code " + res.statusCode + " received");
+                return;
+            }
+
+            res.on("data", function(chunk) {
+                data += chunk;
+            });
+
+            res.on("end", function() {
+                data = new Buffer(data, "binary");
+                done(null, encode(mime, data));
+            });
+        });
+
+        req.on("error", function(err) {
+            done("Unable to convert " + img + ". Error: " + err.code);
+        });
+
+        req.end();
     }
-  });
 
-  /**
-   * Fetches a remote image and encodes it.
-   *
-   * @param img Remote path, like http://url.to/an/image.png
-   * @param done Function to call once done
-   */
-  function fetchImage(img, done) {
-    var opts = url.parse(img);
-
-    var req = http.request(opts, function(res) {
-      res.setEncoding("binary");
-
-      var mime = res.headers["content-type"];
-      var data = "";
-
-      // Bail if we get anything other than 200
-      if(res.statusCode !== 200) {
-        done("Unable to convert " + img + " because the URL did not return an image. Staus code " + res.statusCode + " received");
-        return;
-      }
-
-      res.on("data", function(chunk) {
-        data += chunk;
-      });
-
-      res.on("end", function() {
-        data = new Buffer(data, "binary");
-        done(null, encode(mime, data));
-      });
-    });
-    
-    req.on("error", function(err) {
-      done("Unable to convert " + img + ". Error: " + err.code);
-    });
-
-    req.end();
-  }
-
-  /**
-   * Base64 encodes an image and builds the data URI string
-   *
-   * @param mimeType Mime type of the image
-   * @param img The source image
-   * @return Data URI string
-   */
-  function encode(mimeType, img) {
-    var ret = "data:";
-    ret += mimeType;
-    ret += ";base64,";
-    ret += img.toString("base64");
-    return ret;
-  }
+    /**
+     * Base64 encodes an image and builds the data URI string
+     *
+     * @param mimeType Mime type of the image
+     * @param img The source image
+     * @return Data URI string
+     */
+    function encode(mimeType, img) {
+        var ret = "data:";
+        ret += mimeType;
+        ret += ";base64,";
+        ret += img.toString("base64");
+        return ret;
+    }
 
 
 };
