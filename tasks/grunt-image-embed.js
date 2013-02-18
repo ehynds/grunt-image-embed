d0dfed6bbe55f539aeaf6ea807be89ead8f15c73
ehynds <ehynds@brightcove.com>
2013-02-18 15:53:53 -0500

    grunt 0.4 compatibility. fixes #12, #13

--- tasks/grunt-image-embed.js
+++ tasks/grunt-image-embed.js
@@ -2,12 +2,12 @@
  * Grunt Image Embed
  * https://github.com/ehynds/grunt-image-embed
  *
- * Copyright (c) 2012 Eric Hynds
+ * Copyright (c) 2013 Eric Hynds
  * Licensed under the MIT license.
  */
 
 // Internal libs
-var grunt_encode = require( "./lib/encode" );
+var grunt_encode = require("./lib/encode");
 
 module.exports = function(grunt) {
   "use strict";
@@ -16,41 +16,29 @@ module.exports = function(grunt) {
   var encode = grunt_encode.init(grunt);
 
   // Grunt utils
-  var utils = grunt.utils || grunt.util;
-  var file = grunt.file;
-  var async = utils.async;
-
+  var async = grunt.util.async;
 
   grunt.registerMultiTask("imageEmbed", "Embed images as base64 data URIs inside your stylesheets", function() {
-    var opts = this.data.options;
-    var src = this.file.src;
-    var dest = this.file.dest;
-    var tasks, done;
-
-    if(!src) {
-      grunt.fatal("Missing src property.");
-      return;
-    }
-
-    if(!dest) {
-      grunt.fatal("Missing dest property");
-      return;
-    }
-
-    done = this.async();
+    var opts = this.options();
+    var done = this.async();
 
     // Process each src file
-    tasks = file.expandFiles(src).map(function(srcFile) {
-      return function(callback) {
-        encode.stylesheet(srcFile, opts, callback);
-      };
-    });
+    this.files.forEach(function(file) {
+      var dest = file.dest;
+      var tasks;
 
-    // Once all files have been processed write them out.
-    async.parallel(tasks, function(err, output) {
-      grunt.file.write(dest, output);
-      grunt.log.writeln('File "' + dest + '" created.');
-      done();
+      tasks = file.src.map(function(srcFile) {
+        return function(callback) {
+          encode.stylesheet(srcFile, opts, callback);
+        };
+      });
+
+      // Once all files have been processed write them out.
+      async.parallel(tasks, function(err, output) {
+        grunt.file.write(dest, output);
+        grunt.log.writeln('File "' + dest + '" created.');
+        done();
+      });
     });
   });
 
