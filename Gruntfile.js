9b28c1703be91c42323a1a01dac78ed42e993d05
ehynds <ehynds@brightcove.com>
2014-12-19 15:29:53 -0500

    Clean up the grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,24 @@
 module.exports = function(grunt) {
-
-  // Project configuration.
   grunt.initConfig({
     nodeunit: {
-      all: ["test/test_image_encoder.js", "test/test_css_encoder.js"]
-    },
-    watch: {
-      files: "<%= jshint.tasks.src %>",
-      tasks: ["jshint:tasks", "nodeunit"]
+      all: ['test/*.js']
     },
     jshint: {
       options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        unused: true,
-        node: true
+        jshintrc: true
       },
-      tasks: {
-        src: ["tasks/**/*.js"]
-      },
-      general: {
-        options: { strict: false },
-        src: ["GruntFile.js", "test/**/*.js"]
+      all: {
+        src: [
+          'Gruntfile.js',
+          'tasks/**/*.js',
+          'test/**/*.js'
+        ]
       }
     }
   });
 
-  grunt.loadTasks("tasks");
-  grunt.loadNpmTasks("grunt-contrib-nodeunit");
-  grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask("default", ["jshint", "nodeunit"]);
+  grunt.registerTask('default', ['jshint', 'nodeunit']);
 };
